f273cf530c1725f6547cfc7474db1a19aee63312
Ezequiel Rubio <erubio07@gmail.com>
2026-08-12 16:59:16 -0300

    usuario de dani

--- api/src/controllers/fillUsers.js
+++ api/src/controllers/fillUsers.js
@@ -10,6 +10,14 @@ const fillUser = async () => {
       password: "2423122621",
     });
     await user1.setRole(1);
+    const user2 = await User.create({
+      name: "Daniel",
+      surname: "Zaneti",
+      mail: "dzaneti@gmail.com",
+      username: "dzaneti",
+      password: "zaneti123",
+    });
+    await user2.setRole(1);
   } catch (error) {
     throw new Error(error.message);
   }
